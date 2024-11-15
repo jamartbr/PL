4d6f824ca46d1d032dc64a0b8c02bc66476ce60c
Jaime Martinez <jamartbr@gmail.com>
2024-11-15 12:48:08 +0100

    inicio P3

---
+++ P3/main.c
@@ -0,0 +1,53 @@
+// ************************************************************************
+// ***
+// *** Fichero : MAIN .C
+// *** Funcion : Pruebas de FLEX / BISON para prácticas de PL
+// ***
+// ************************************************************************
+
+# include <stdio.h>
+# include <stdlib.h>
+
+extern FILE * yyin ;
+extern int yylineno ;
+
+int yyparse (void) ;
+
+// ************************************************************************
+
+void yyerror (const char *s)
+{
+    fprintf (stderr, "[Línea %2d] *** %s\n", yylineno , s);
+}
+
+// ************************************************************************
+
+FILE *abrir_entrada (int argc, char *argv [])
+{
+    FILE *f= NULL ;
+
+    if (argc >1)
+    {
+        f= fopen( argv [1] ,"r") ;
+
+        if (f== NULL)
+        {
+            fprintf (stderr , " Error : Archivo ' %s' no encontrado \n", argv [1]) ;
+            exit (1) ;
+        }
+        else
+            printf ("\nLeyendo fichero ' %s '\n", argv [1]) ;
+    }
+    else
+        printf ("\nLeyendo entrada standard .\n");
+
+    return (f) ;
+}
+
+// ************************************************************************
+
+int main ( int argc, char *argv [] )
+{
+    yyin = abrir_entrada (argc, argv ) ;
+    return yyparse() ;
+}
